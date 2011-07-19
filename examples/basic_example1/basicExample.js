8e49353e7cd22f68a997115dc40b33db666a08f7
Josh Bennett <josh@j-ben.com>
2011-07-19 16:20:38 -0400

    fixed example

--- examples/basic_example1/basicExample.js
+++ examples/basic_example1/basicExample.js
@@ -8,7 +8,7 @@
 		
 		var formula_1 = 'SUM((A1:A10>=5)*(A1:A10<=10)*A1:A10)',
 			formula_2 = '=RIGHT(A2,LEN(A2)-FIND("*",SUBSTITUTE(A2," ","*",LEN(A2)-LEN(SUBSTITUTE(A2," ","")))))',
-			formula_3 = '=IF(A2:A3="YES","A2 equals yes", "A2 does not equal yes")';
+			formula_3 = '=IF(A2:A3="YES","A2 equals yes", "A2 does not equal yes")',
 			formula_4 = 'ADDRESS(ROW(DataRange2),COLUMN(DataRange2),4)&":"&ADDRESS(MAX((DataRange2<>"")*ROW(DataRange2)),COLUMN(DataRange2)+COLUMNS(DataRange2)-1,4)';
 		
 		document.getElementById('fomatFormula_1').innerHTML = formula_1;
