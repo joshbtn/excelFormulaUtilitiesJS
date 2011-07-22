99fe8fdd7f1fbc078766f66984ce80178e05906f
Josh Bennett <josh@j-ben.com>
2011-07-22 11:33:33 -0400

    cleaned up beautifier example

--- examples/beautifier/beautifier.js
+++ examples/beautifier/beautifier.js
@@ -1,26 +1,27 @@
-(function(){
-	window.excelFormulaBeautifier = window.excelFormulaBeautifier || {};
-	window.excelFormulaBeautifier.examples = window.excelFormulaBeautifier.examples || {};
-	var beautifier = window.excelFormulaBeautifier.examples.beautifier = {},
-		formula,
-		input,
-		formulaTitle,
-		formulaBody;
-	
-	var update = beautifier.update = function(){
-		formula = input.value;
-		formulaTitle.innerHTML = formula;
-		formulaBody.innerHTML = excelFormulaUtilities.formatFormulaHTML(formula);
-	}
-	
-	window.onload = function(){
-		formula = '';
-		input = document.getElementById('formula_input');
-		formulaTitle = document.getElementById('fomatFormula_2');
-		formulaBody = document.getElementById('fomatFormula_2_out');
-		
-		input.value = formula
-		
-		update();
-	}
-}())
+(function () {
+    "use strict";
+    window.excelFormulaBeautifier = window.excelFormulaBeautifier || {};
+    window.excelFormulaBeautifier.examples = window.excelFormulaBeautifier.examples || {};
+
+    var beautifier = window.excelFormulaBeautifier.examples.beautifier = {},
+		formula, 
+		input, 
+		formulaTitle, 
+		formulaBody,
+		update = beautifier.update = function () {
+            formula = input.value;
+            formulaTitle.innerHTML = formula;
+            formulaBody.innerHTML = window.excelFormulaUtilities.formatFormulaHTML(formula);
+        };
+
+    window.onload = function () {
+        formula = '';
+        input = document.getElementById('formula_input');
+        formulaTitle = document.getElementById('fomatFormula_2');
+        formulaBody = document.getElementById('fomatFormula_2_out');
+
+        input.value = formula;
+
+        update();
+    };
+}());
