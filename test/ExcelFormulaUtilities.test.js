be698d111846b8c435ac6bed1b9553a25a207f19
Josh Bennett <josh@j-ben.com>
2012-10-10 21:10:49 -0400

    fixed the qunit tests

--- test/ExcelFormulaUtilities.test.js
+++ test/ExcelFormulaUtilities.test.js
@@ -11,12 +11,12 @@ test("Test formatFormula ()", function() {
 
 test("formatFormula", function() {
   var inputFormula = 'IF("foo" = "foo", "foo", "bar")';
-  var excpected = 'IF\n(\n\t"foo"= "foo",\n\t"foo",\n\t"bar"\n)';
+  var excpected = '=IF(\n\t"foo"= "foo",\n\t"foo",\n\t"bar"\n)';
   
   equal(window.excelFormulaUtilities.formatFormula(inputFormula), excpected, "Simple formating example.");
   
-  inputFormula = 'IF(R[39]C[11]>65,R[25]C[42],ROUND((R[11]C[11]*IF(OR(AND(R[39]C[11]>=55, R[40]C[11]>=20),AND(R[40]C[11]>=20,R11C3="YES")),R[44]C[11],R[43]C[11]))+(R[14]C[11] *IF(OR(AND(R[39]C[11]>=55,R[40]C[11]>=20),AND(R[40]C[11]>=20,R11C3="YES")), R[45]C[11],R[43]C[11])),0))'
-  excpected = 'IF\n(\n\tR[39]C[11]> 65,\n\tR[25]C[42],\n\tROUND\n\t(\n\t\t( R[11]C[11]*\n\t\t\tIF\n\t\t\t(\n\t\t\t\tOR\n\t\t\t\t(\n\t\t\t\t\tAND\n\t\t\t\t\t(\n\t\t\t\t\t\tR[39]C[11]>= 55,\n\t\t\t\t\t\tR[40]C[11]>= 20\n\t\t\t\t\t),\n\t\t\t\t\tAND\n\t\t\t\t\t(\n\t\t\t\t\t\tR[40]C[11]>= 20,\n\t\t\t\t\t\tR11C3= "YES"\n\t\t\t\t\t)\n\t\t\t\t),\n\t\t\t\tR[44]C[11],\n\t\t\t\tR[43]C[11]\n\t\t\t) )+ ( R[14]C[11]*\n\t\t\tIF\n\t\t\t(\n\t\t\t\tOR\n\t\t\t\t(\n\t\t\t\t\tAND\n\t\t\t\t\t(\n\t\t\t\t\t\tR[39]C[11]>= 55,\n\t\t\t\t\t\tR[40]C[11]>= 20\n\t\t\t\t\t),\n\t\t\t\t\tAND\n\t\t\t\t\t(\n\t\t\t\t\t\tR[40]C[11]>= 20,\n\t\t\t\t\t\tR11C3= "YES"\n\t\t\t\t\t)\n\t\t\t\t),\n\t\t\t\tR[45]C[11],\n\t\t\t\tR[43]C[11]\n\t\t\t) ),\n\t\t0\n\t)\n)';
+  inputFormula = 'IF(A1="yes", "yes", "no")'
+  excpected = '=IF(\n\tA1 = "yes",\n\t,"yes",\n\t"no"\n)';
   equal(excelFormulaUtilities.formatFormula(inputFormula), excpected, "advanced example.");
  
 });
@@ -58,23 +58,23 @@ test("formula2CSharp", function() {
 	
 	inputFormula = 'IF(FOO_BAR = "foo bar", "THIS WORKED", "THIS ISN\'T WORKING")';
 	excpected = '(FOO_BAR=="foo bar"?"THIS WORKED":"THIS ISN\'T WORKING")';
-	equal(excelFormulaUtilities.formula2JavaScript(inputFormula), excpected, "Test that strings keep correct spaces. See issue #2. https://github.com/joshatjben/excelFormulaUtilitiesJS/issues/2");
+	equal(excelFormulaUtilities.formula2CSharp(inputFormula), excpected, "Test that strings keep correct spaces. See issue #2. https://github.com/joshatjben/excelFormulaUtilitiesJS/issues/2");
 });
 
 test("formula2JavaScript", function() {
 	var inputFormula = 'IF("foo" = "foo", "foo", "bar")',
-		excpected = '("foo"=="foo"?"foo":"bar")';
+		excpected = '("foo"==="foo"?"foo":"bar")';
 	equal(excelFormulaUtilities.formula2JavaScript(inputFormula), excpected, "Simple if example.");
   
 	inputFormula = 'IF(IF(true, "foo", "bar") = "foo", "foo", "bar")';
-	excpected = '((true?"foo":"bar")=="foo"?"foo":"bar")';
+	excpected = '((true?"foo":"bar")==="foo"?"foo":"bar")';
 	equal(excelFormulaUtilities.formula2JavaScript(inputFormula), excpected, "Nested If Test.");
 	
 	inputFormula = 'IF(IF(MAX(1, -10)>0, "foo", "bar") = "foo", "foo", "bar")';
-	excpected = '((Math.Max(1,-10)>0?"foo":"bar")=="foo"?"foo":"bar")';
+	excpected = '((Math.Max(1,-10)>0?"foo":"bar")==="foo"?"foo":"bar")';
 	equal(excelFormulaUtilities.formula2JavaScript(inputFormula), excpected, "Nested If Test with a nested function.");
 	
 	inputFormula = 'IF(FOO_BAR = "foo bar", "THIS WORKED", "THIS ISN\'T WORKING")';
-	excpected = '(FOO_BAR=="foo bar"?"THIS WORKED":"THIS ISN\'T WORKING")';
+	excpected = '(FOO_BAR==="foo bar"?"THIS WORKED":"THIS ISN\'T WORKING")';
 	equal(excelFormulaUtilities.formula2JavaScript(inputFormula), excpected, "Test that strings keep correct spaces. See issue #2. https://github.com/joshatjben/excelFormulaUtilitiesJS/issues/2");
 });
