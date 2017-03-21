6c52ae450dbfdc9bfa385088a4c8e0d3660b76e0
Josh Bennett <Josh@j-ben.com>
2017-03-20 22:33:51 -0400

    resolves issue #54
    Add clipboard.js library
    Add copy button to formula output area

--- js/page.js
+++ js/page.js
@@ -1,13 +1,37 @@
-(function () {
-    "use strict";
-    
-    window.addEventListener('load', function(){
-        var isEu = window.document.getElementById("isEu");
-        
-        isEu.addEventListener('click', function(){
-            window.excelFormulaUtilities.isEu = isEu.checked;
-        });
-        
+(function() {
+  "use strict";
+  window.addEventListener('load', function() {
+    var isEu = window.document.getElementById("isEu");
+    isEu.addEventListener('click', function() {
+      window.excelFormulaUtilities.isEu = isEu.checked;
     });
-    
-}())
+  });
+
+  function selectText(container) {
+    if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(container);
+      range.select();
+    } else if (window.getSelection) {
+      window.getSelection().empty()
+      var range = document.createRange();
+      range.selectNode(container);
+      window.getSelection().addRange(range);
+    }
+  }
+
+  var clipboard = new Clipboard('#copyFormulaBtn', {
+    text: function(trigger) {
+        return document.getElementById('fomatFormula_2_out').innerHtml;
+    }
+  });
+
+  clipboard.on('success', function(e) {
+    e.clearSelection();
+  });
+
+  clipboard.on('error', function(e) {
+  });
+
+
+}());
