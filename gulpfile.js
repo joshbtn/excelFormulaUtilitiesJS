85635f3645741fd9a8d8023a41b30b23c8038d70
Josh Bennett <Josh@j-ben.com>
2017-03-20 23:51:58 -0400

    updated css classes for new version of bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,34 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     concat = require('gulp-concat'),
     del = require('del'),
+    fs = require('fs.extra'),
     paths = {
       scripts: ['src/core.js', 'src/ExcelFormulaUtilities.js'],
-      siteScripts: ['js/*.js'],
+      siteScripts: ['js/page.js', 'js/beautifier.js'],
       css: ['bootstrap.min.css', 'css/*.css']
     };
 
-gulp.task('libs', function(){
-  gulp.src('node_modules/bootstrap/dist/js/*.min.js')
+gulp.task('update_jquery', function(){
+  return gulp.src('node_modules/jquery/dist/jquery.min.*')
     .pipe(gulp.dest('js'));
-  gulp.src('node_modules/jquery/dist/jquery.min.*')
-    .pipe(gulp.dest('js'));
-  gulp.src('node_modules/bootstrap/dist/css/*.min.css')
+})
+
+gulp.task('update_bs_css', function(){
+  return gulp.src('node_modules/bootstrap/dist/css/*.min.css')
     .pipe(gulp.dest('css'));
-  gulp.src('node_modules/bootstrap/dist/fonts/*')
+});
+
+gulp.task('update_bs_js', function(){
+  return gulp.src('node_modules/bootstrap/dist/js/*.min.js')
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('update_bs_fonts', function(){
+  return gulp.src('node_modules/bootstrap/dist/fonts/*')
     .pipe(gulp.dest('fonts'));
 });
+
+gulp.task('libs', ['update_jquery', 'update_bs_js', 'update_bs_css', 'update_bs_fonts']);
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
@@ -32,13 +44,16 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('site', function() {
-  return gulp.src(paths.siteScripts)
+  gulp.src(paths.siteScripts)
       .pipe(concat('page.js'))
       .pipe(concat('beautifier.js'))
-      .pipe(gulp.dest('js'))
+      .pipe(gulp.dest('js/tmp'))
       .pipe(rename({suffix: '.min'}))
       .pipe(uglify())
       .pipe(gulp.dest('js'))
+      .on('end', function(){
+        fs.rmrfSync('js/tmp/')
+      })
 });
 
 gulp.task('watch', function() {
