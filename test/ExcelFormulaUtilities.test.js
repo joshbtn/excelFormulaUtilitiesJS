caa54fb85b35f233dfc7f3604d0a00105e8377ad
Josh Bennett <Josh@j-ben.com>
2017-04-26 21:35:50 -0400

    Test re-org

--- test/ExcelFormulaUtilities.test.js
+++ test/ExcelFormulaUtilities.test.js
@@ -1,26 +1,50 @@
-var assert = require('assert');
+"use strict";
 
-var formula = require('../index.js');
+let assert = require('assert');
 
-describe("Test parsing a formula that does not start with a function. From Issue #46", function(){
-  var inputFormula='=A1+B1',
-  expected = 'A1 + B1',
-  actual = formula.formatFormula(inputFormula);
-  assert.equal(actual, expected);
+let formula = require('../index.js');
+describe("ExcelFormulaUtilities", () => {
 
-  inputFormula='=A5';
-  expected = 'A5';
-  actual = formula.toCSharp(inputFormula);
+  describe("#formatFormula", () => {
 
-  assert.equal(actual, expected);
+    it("should replace < or > signs in a formula with &lt; and &gt;.  Tests fix for Issue #58.", () => {
+      assert.equals(true, false)
+    });
 
-  inputFormula='=A1+B1';
-  expected = 'A1+B1';
-  actual = formula.toJavaScript(inputFormula);
+    it("should parse a formula that doesn't start with a function. Tests fix for Issue #46.", () => {
+      let inputFormula='=A1+B1';
+      let expected = 'A1 + B1';
+      let actual = formula.formatFormula(inputFormula);
+      assert.equal(actual, expected);
+    });
 
-  inputFormula='=A1+B1';
-  expected = 'A1+B1';
-  actual = formula.toPython(inputFormula);
+  });
 
-  assert.equal(actual, expected);
+  describe("#toCSharp", () => {
+    it("should parse a formula that doesn't start with a function. Tests fix for Issue #46.", () => {
+      let inputFormula='=A5';
+      let expected = 'A5';
+      let actual = formula.toCSharp(inputFormula);
+      assert.equal(actual, expected);
+    });
+  });
+
+  describe("#toJavaScript", () => {
+    it("should parse a formula that doesn't start with a function. Tests fix for Issue #46.", () => {
+      let inputFormula='=A1+B1';
+      let expected = 'A1+B1';
+      let actual = formula.toJavaScript(inputFormula);
+      assert.equal(actual, expected);
+    })
+  })
+
+  describe("#toPython", () => {
+    it("Test parsing a formula that does not start with a function. From Issue #46", () => {
+      let inputFormula='=A1+B1';
+      let expected = 'A1+B1';
+      let actual = formula.toPython(inputFormula);
+      assert.equal(actual, expected);
+    })
+  })
+
 })
